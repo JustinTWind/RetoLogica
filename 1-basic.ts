0cbc7e2aa8903dd2a0e98419eb7030a800f3579b
JustinTWind <justin.20@outlook.com>
2026-03-03 23:29:40 -0500

    Initial Commit

---
+++ 1-basic.ts
@@ -0,0 +1,22 @@
+/*
+Ejercicio 1 (Básico)
+  
+Descripción
+  Dado un arreglo de enteros precios, encuentra los 3 precios más altos y muéstralos en orden descendente.
+  Si el arreglo tiene menos de 3 elementos, devuelve todos los elementos ordenados de mayor a menor.
+
+No está permitido usar funciones de ordenamiento del lenguaje:
+  JavaScript: Array.sort()
+  Java: Arrays.sort() / Collections.sort()
+
+Objetivo de aprendizaje
+  Recorrer arreglos
+  Comparar valores
+  Mantener el “Top 3” actualizado con lógica condicional
+  Modularizar en funciones (por ejemplo: insertar en top)
+
+Restricciones
+  1 ≤ n ≤ 100000
+  0 ≤ precios[i] ≤ 10^9
+
+*/
